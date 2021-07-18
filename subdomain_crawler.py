85c0e599257e23ddfa0f9f49d5fc3267a124b17b
Souvik Chatterjee <55107082+codassassin@users.noreply.github.com>
2021-07-18 22:16:15 +0530

    subdomain_crawl.py

---
+++ subdomain_crawler.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+target_url = input("Enter site for crawling (e.g. google.com):")
+subdomains = []
+
+with open("subdomains.txt", 'r') as file:
+    for line in file:
+        word = line.strip()
+        test_url = word + "." + target_url
+        response = request(test_url)
+        subdomains.append("https://" + test_url)
+
+        if response:
+            print("Subdomain {} exists!!".format("https://" + test_url))
+
+with open("subdomain.csv", "r+") as f:
+    f.writelines(f'\n{target_url}, {subdomains}')
